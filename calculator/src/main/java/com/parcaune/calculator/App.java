b6197f4078dbb3facc2167ed91ae5607fe7879f3
rosinetiani <rosine.tiani@parcaune.com>
2021-03-05 15:55:54 +0100

    creation of a simple maven project

---
+++ calculator/src/main/java/com/parcaune/calculator/App.java
@@ -0,0 +1,11 @@
+package com.parcaune.calculator;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        System.out.println("Addition of 5 + 4 = " + calculator.addition(5, 4));
+    }
+}
---
+++ calculator/src/main/java/com/parcaune/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package com.parcaune.calculator;
+
+public class Calculator {
+
+    Integer addition(Integer x1, Integer x2) {
+        return x1 + x2;
+    }
+}
+
