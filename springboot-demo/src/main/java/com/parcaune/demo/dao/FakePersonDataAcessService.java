a76db0c1d5395a82006b0332ac9fbd2654c0a01f
rosinetiani <rosine.tiani@parcaune.com>
2021-03-17 00:15:27 +0100

    the class FakePersonDataAcessServiceb was transformed into repository

--- springboot-demo/src/main/java/com/parcaune/demo/dao/FakePersonDataAcessService.java
+++ springboot-demo/src/main/java/com/parcaune/demo/dao/FakePersonDataAcessService.java
@@ -1,18 +1,26 @@
 package com.parcaune.demo.dao;
 
 import com.parcaune.demo.model.Person;
-import org.graalvm.compiler.nodes.java.ArrayLengthNode;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+
+@Repository("fakeDao")
 public class FakePersonDataAcessService implements PersonDao {
     private static List<Person> DB =new ArrayList<>();
 
     @Override
     public int insertPerson(UUID id, Person person) {
-        DB.add(new Person(id, person.getName()));
+        Person personToInsert = new Person(id, person.getName());
+        DB.add(personToInsert);
         return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
     }
 }
