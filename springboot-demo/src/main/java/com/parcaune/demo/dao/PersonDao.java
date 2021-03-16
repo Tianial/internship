0592b9302b1eeadab819e9fb5c230e2e357a801e
rosinetiani <rosine.tiani@parcaune.com>
2021-03-17 00:20:50 +0100

    methhod addPerson was changed to insertPerson and the list of selectAllPeople was added

--- springboot-demo/src/main/java/com/parcaune/demo/dao/PersonDao.java
+++ springboot-demo/src/main/java/com/parcaune/demo/dao/PersonDao.java
@@ -2,15 +2,17 @@ package com.parcaune.demo.dao;
 
 import com.parcaune.demo.model.Person;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PersonDao {
 
     int insertPerson(UUID id, Person person);
 
-    default int addPerson(Person person) {
+    default int insertPerson(Person person) {
         UUID id = UUID.randomUUID();
         return insertPerson(id,person);
     }
+    List<Person> selectAllPeople();
 
 }
