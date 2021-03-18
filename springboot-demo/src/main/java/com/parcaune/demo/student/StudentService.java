fba943a70363666250984ae6114ef5dacddac8fe
rosinetiani <rosine.tiani@parcaune.com>
2021-03-18 13:35:16 +0100

    creation and implementation of the class StudentService ,its attributes and methods.Also the addition of the properties @service for it to be recognised by springboot

---
+++ springboot-demo/src/main/java/com/parcaune/demo/student/StudentService.java
@@ -0,0 +1,17 @@
+package com.parcaune.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents(){
+        return List.of(new Student(1L,"Tiani","tiani@parcaune.com", LocalDate.of(2018, Month.JUNE,21),3));
+    }
+
+}
