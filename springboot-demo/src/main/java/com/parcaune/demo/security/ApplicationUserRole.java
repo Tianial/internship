d3574fd00d7a2dc1ae4e142e55c5fda0c19264e1
rosinetiani <rosine.tiani@parcaune.com>
2021-04-06 00:17:26 +0200

    assertions of authorisation to the persons in the project

--- springboot-demo/src/main/java/com/parcaune/demo/security/ApplicationUserRole.java
+++ springboot-demo/src/main/java/com/parcaune/demo/security/ApplicationUserRole.java
@@ -1,23 +1,34 @@
 package com.parcaune.demo.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.parcaune.demo.security.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
     STUDENT(Sets.newHashSet()),
-    ADMIN(Sets.newHashSet(STUDENT_READ,STUDENT_WRITE,COURSE_WRITE,COURSE_READ)),
-    ADMINTRAINEE(Sets.newHashSet(STUDENT_READ,COURSE_READ));
+    ADMIN(Sets.newHashSet(STUDENT_READ, STUDENT_WRITE, COURSE_WRITE, COURSE_READ)),
+    ADMINTRAINEE(Sets.newHashSet(STUDENT_READ, COURSE_READ));
 
-    private final Set<ApplicationUserPermission> Permissions;
+    private final Set<ApplicationUserPermission> permissions;
 
     ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
-        Permissions = permissions;
+        this.permissions = permissions;
     }
 
     public Set<ApplicationUserPermission> getPermissions() {
-        return Permissions;
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermissions()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("role" + this.name()));
+        return permissions;
     }
 }
