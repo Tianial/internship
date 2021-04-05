3540e8d2500e9058436cfc60bf7f07b91976db23
rosinetiani <rosine.tiani@parcaune.com>
2021-04-06 00:12:24 +0200

    method getStudentById created and implemented for throwing exceptions

--- springboot-demo/src/main/java/com/parcaune/demo/student/StudentService.java
+++ springboot-demo/src/main/java/com/parcaune/demo/student/StudentService.java
@@ -1,10 +1,13 @@
 package com.parcaune.demo.student;
 
+import com.parcaune.demo.exceptions.StudentAppEntityNotFoundException;
+import com.sun.xml.bind.v2.model.core.ID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -19,9 +22,18 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
+    public Student getStudentById(Long id) {
+        Optional<Student> optionalStudent = studentRepository.findById(id);
+        if (optionalStudent.isPresent()) {
+            return optionalStudent.get();
+        } else {
+            throw new StudentAppEntityNotFoundException(String.format("Student with [%d] unfortunately not found", id));
+        }
+    }
 
     public void addNewStudent(Student student) {
         System.out.println(student);
         studentRepository.save(student);
     }
+
 }
