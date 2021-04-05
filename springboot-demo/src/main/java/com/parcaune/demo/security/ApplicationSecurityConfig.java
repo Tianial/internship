d9e478f3b1a5e663ee3eec08d8bd669c16df90c0
rosinetiani <rosine.tiani@parcaune.com>
2021-04-06 00:14:29 +0200

    new role ADMINTRAINEE added and addition of anmatchers in other to limit the access on the URL

--- springboot-demo/src/main/java/com/parcaune/demo/security/ApplicationSecurityConfig.java
+++ springboot-demo/src/main/java/com/parcaune/demo/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.parcaune.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.parcaune.demo.security.ApplicationUserPermission.*;
+import static com.parcaune.demo.security.ApplicationUserRole.*;
 import static com.parcaune.demo.security.ApplicationUserRole.STUDENT;
 
 @Configuration
@@ -29,9 +32,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()// TODO
                 .authorizeRequests()
+                /*
                 .antMatchers("/","index","/css/*","/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())  // will protect the url(api for students(Role based authentification))
+                .antMatchers("/management/**").hasRole(STUDENT.name())  // will protect the url(api for students(Role based authentification))
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermissions()) // the path hier should have the COURSE_WRITE authority(role) thanks to antmatchers
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermissions()) // will protect the url(api for students(Role based authentification))
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermissions())// will protect the url(api for students(Role based authentification))
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMINTRAINEE.name(), ADMIN.name())
+
+                 */
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -46,19 +58,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .username("Monthe")
                 .password(passwordEncoder.encode("password"))
                 .roles(STUDENT.name()) //spring understands this line as ROLE_STUDENT
+               .authorities(STUDENT.getGrantedAuthorities())
                .build();
 
 
         UserDetails tianiuser = User.builder()
                 .username("Tiani")
                 .password(passwordEncoder.encode("Tiani21"))
-                .roles(ApplicationUserRole.ADMIN.name()) // Role ADMIN
+                .roles(ADMIN.name()) // Role ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails Aliceuser = User.builder()
                 .username("Alice")
                 .password(passwordEncoder.encode("Alice21"))
-                .roles(ApplicationUserRole.ADMINTRAINEE.name()) //Role Admintrainee
+                .roles(ADMINTRAINEE.name()) //Role Admintrainee
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
 
