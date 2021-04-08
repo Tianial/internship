49b55496b95d1c5fd86db6b0c478e9e252ca2373
rosinetiani <rosine.tiani@parcaune.com>
2021-04-08 23:02:51 +0200

    since we removed the antmatchers, we used another method to securise it.ie the @PreAuthorise

--- springboot-demo/src/main/java/com/parcaune/demo/student/StudentManagementController.java
+++ springboot-demo/src/main/java/com/parcaune/demo/student/StudentManagementController.java
@@ -1,7 +1,9 @@
 package com.parcaune.demo.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
@@ -17,25 +19,29 @@ public class StudentManagementController {
     );
 
     @GetMapping
-    public static List<Student> getSTUDENTS() {
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
+    public static List<Student> getSTUDENTS(Principal user) {
         System.out.println("getSTUDENTS");
         return STUDENTS;
     }
 
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student : write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println("registerNewStudent");
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student : write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId ){
         System.out.println("deleteStudent");
         System.out.println(studentId);
     }
 
     @PutMapping (path = "{studentId}")
+    @PreAuthorize("hasAuthority('student : write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student){
         System.out.println("updateStudent");
         System.out.println(String.format("%s,%s",studentId,student));
