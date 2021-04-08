a9b4a676fdd6266ada4ff38fc0dd57c62c0816d7
rosinetiani <rosine.tiani@parcaune.com>
2021-04-08 22:52:42 +0200

    thhe basic authentification was changed to form based authentification and antmachters removed and replaced by preAuthorisers

--- springboot-demo/src/main/java/com/parcaune/demo/security/ApplicationSecurityConfig.java
+++ springboot-demo/src/main/java/com/parcaune/demo/security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.parcaune.demo.security.ApplicationUserPermission.*;
 import static com.parcaune.demo.security.ApplicationUserRole.*;
@@ -19,6 +21,7 @@ import static com.parcaune.demo.security.ApplicationUserRole.STUDENT;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)   //permitts preAuthorisation
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private  final PasswordEncoder passwordEncoder;
@@ -32,22 +35,27 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()// TODO
+                //.csrf().disable()// TODO
+                //csrf generation
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeRequests()
-                /*
                 .antMatchers("/","index","/css/*","/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())  // will protect the url(api for students(Role based authentification))
-                .antMatchers("/management/**").hasRole(STUDENT.name())  // will protect the url(api for students(Role based authentification))
+/*
                 .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermissions()) // the path hier should have the COURSE_WRITE authority(role) thanks to antmatchers
                 .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermissions()) // will protect the url(api for students(Role based authentification))
                 .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermissions())// will protect the url(api for students(Role based authentification))
                 .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMINTRAINEE.name(), ADMIN.name())
-
-                 */
+**/
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+               // .httpBasic();  // Basic Authentification
+                .formLogin() // form based authentification
+                .loginPage("/login").permitAll(); //introducing a new login page in App and permitting all urls
+
+
     }
 
     @Override
@@ -57,7 +65,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
        UserDetails montheuser = User.builder()
                 .username("Monthe")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name()) //spring understands this line as ROLE_STUDENT
+                //.roles(STUDENT.name()) //spring understands this line as ROLE_STUDENT
                .authorities(STUDENT.getGrantedAuthorities())
                .build();
 
@@ -65,23 +73,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails tianiuser = User.builder()
                 .username("Tiani")
                 .password(passwordEncoder.encode("Tiani21"))
-                .roles(ADMIN.name()) // Role ADMIN
+                //.roles(ADMIN.name(), ADMINTRAINEE.name()) // Role ADMIN
                 .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails Aliceuser = User.builder()
                 .username("Alice")
                 .password(passwordEncoder.encode("Alice21"))
-                .roles(ADMINTRAINEE.name()) //Role Admintrainee
+                //.roles(ADMINTRAINEE.name()) //Role Admintrainee
                 .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
 
         return new InMemoryUserDetailsManager(montheuser,tianiuser,Aliceuser);
-
-
-
-
     }
 
 
