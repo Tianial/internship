4decbe8096048a74f085d09bbb3ae397eb012de7
rosinetiani <rosine.tiani@parcaune.com>
2021-04-14 23:50:03 +0200

    comments added and changed in oder for the concepts of annotations to be clearer

--- springboot-demo/src/main/java/com/parcaune/demo/student/Student.java
+++ springboot-demo/src/main/java/com/parcaune/demo/student/Student.java
@@ -4,13 +4,13 @@ package com.parcaune.demo.student;
 import javax.persistence.*;
 import java.time.LocalDate;
 
-@Entity
+@Entity  //takes a row in the table of Db and maps or transforms in to the class Student in Java
 @Table   // for tables in DB
 public class Student {
 
     @GeneratedValue(strategy=GenerationType.AUTO)     //generates automatically id of type LONG for every saved user
-    @Id  // it indicates the attribute id in the DB
-    private Long id;
+    @Id  // it indicates the attribute id in the DB (le champs sur lequell sa se trouve est ma cle primaire
+    private Long id; // permits thanks to the @Id to recognise each of the rows in a unique manner
 
     private String name;
     private String email;
