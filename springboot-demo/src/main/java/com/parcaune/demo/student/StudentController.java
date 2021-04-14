a343964ef240a25a711f4edd5b2689aeb97fd34c
rosinetiani <rosine.tiani@parcaune.com>
2021-04-14 23:58:15 +0200

    function of deleting students implemented, and commend added on autowire

--- springboot-demo/src/main/java/com/parcaune/demo/student/StudentController.java
+++ springboot-demo/src/main/java/com/parcaune/demo/student/StudentController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.List;
 
+//recieves the Http requests and sends it to the service to executes it
+
 
 @RestController  // for it to undergo http request
 @RequestMapping(path = "/api/v1/student")
@@ -13,13 +15,13 @@ public class StudentController {
 
     private final StudentService studentService;
 
-    @Autowired
+    @Autowired  // used here to instantiate studentService created above
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
     @GetMapping()
-    public List<Student> getStudents(Principal user) {
+    public List<Student> getStudents() {
         return studentService.getStudents();
     }
 
@@ -37,6 +39,7 @@ public class StudentController {
     public void deleteStudent(@PathVariable("studentId") Long studentId ){
         System.out.println("deleteStudent");
         System.out.println(studentId);
+        studentService.deleteStudent(studentId);
     }
 
 }
