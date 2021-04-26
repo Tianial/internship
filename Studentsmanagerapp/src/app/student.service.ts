39b9807b63b469de012bb7f30a3d7442f293b302
rosinetiani <rosine.tiani@parcaune.com>
2021-04-26 13:36:45 +0200

    path of the methods edit,delete,create and have the students list determined

---
+++ Studentsmanagerapp/src/app/student.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Student} from "./student";
+import {environment} from "../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  private apiServiceUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+  /**
+   * getStudent returns an Observable of type Student[]
+   */
+  public getStudent(): Observable<Student[]> {
+    return this.http.get<Student[]>(`${this.apiServiceUrl}/student`);
+  }
+
+  public addStudent(student: Student): Observable<Student> {
+    return this.http.post<Student>(`${this.apiServiceUrl}'/api/student`, student);
+  }
+
+
+  public updateStudent(student: Student): Observable<Student> {
+    return this.http.put<Student>(`${this.apiServiceUrl}'/student`, student);
+  }
+
+  public deleteStudent(studentId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServiceUrl}'/student/${studentId}`);
+  }
+
+}
+
