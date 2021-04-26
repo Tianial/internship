59ecf35977408758e08945ef290a18bd16af5d54
rosinetiani <rosine.tiani@parcaune.com>
2021-04-26 13:25:32 +0200

    attribute added to the constructor

--- springboot-demo/src/main/java/com/parcaune/demo/student/StudentConfig.java
+++ springboot-demo/src/main/java/com/parcaune/demo/student/StudentConfig.java
@@ -15,9 +15,9 @@ public class StudentConfig {
     CommandLineRunner commandLineRunner(StudentRepository studentRepository)  // by givx  as parameter studentRepository of type StudentRepository, we are injecting the DB hier for the informations in bracket to be stored
     {
         return (args) -> {
-            Student tiani =new Student("tiani","tiani@parcaune.com", LocalDate.of(2017, Month.JUNE,21),4);  // id is removed b/c it is generated automatically from the DB
+            Student tiani =new Student("tiani","tiani@parcaune.com", LocalDate.of(2017, Month.JUNE,21),4,"234567876543","assets/images/pic3.jpeg","678","Head of class");  // id is removed b/c it is generated automatically from the DB
 
-            Student alice = new Student("alice","alice@parcaune.com", LocalDate.of(2018, Month.JUNE,21),3);
+            Student alice = new Student("alice","alice@parcaune.com", LocalDate.of(2018, Month.JUNE,21),3,"3456789","assets/images/pic1.jpeg","564","Delegate");
 
             studentRepository.saveAll(List.of(tiani,alice));  //to save the student list in the DB.//creates an array or list with tiani and alice inside
         };
