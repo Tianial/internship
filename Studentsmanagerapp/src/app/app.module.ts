70f80b64cb97ebbde7ac050def922533404d9e69
rosinetiani <rosine.tiani@parcaune.com>
2021-04-29 11:00:30 +0200

    New component created

--- Studentsmanagerapp/src/app/app.module.ts
+++ Studentsmanagerapp/src/app/app.module.ts
@@ -5,10 +5,12 @@ import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {StudentService} from "./student.service";
 import {FormsModule} from "@angular/forms";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
