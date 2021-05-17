a9045fa1999ca93a3641e47e3c4e2f86c65f3ff3
rosinetiani <rosine.tiani@parcaune.com>
2021-05-17 17:11:31 +0200

    class selectExistsEmail created for tests in Repository

---
+++ springboot-demo/src/test/java/com/parcaune/demo/student/StudentRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.parcaune.demo.student;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+class StudentRepositoryTest  // Test in Repository
+{
+
+    @Autowired
+    private StudentRepository underTests;
+
+    @AfterEach
+    void tearDown() {
+        underTests.deleteAll();
+
+    }
+
+    @Test
+    void itShouldCheckIfStudentEmailExists(){
+        //given
+
+        Student student=new Student(
+                "Tiani","tiani@parcaune.com", LocalDate.of(2017, Month.JUNE,21),4,"345678987654","assets/images/pic3.jpeg","456","student"
+        );
+
+        underTests.save(student); // saves all students
+
+        //when
+        String email="tiani@parcaune.com";
+        Boolean expected= underTests.selectExistsEmail(email); // selectExistsEmail(email) is being tested
+
+
+        //then
+
+        assertThat(expected).isTrue();
+
+    }
+
+    @Test
+    void itShouldCheckIfStudentEmailDoesnotExists(){
+        //given
+
+        String email="tiani@parcaune.com";
+
+
+        Boolean expected= underTests.selectExistsEmail(email); // selectExistsEmail(email) is being tested
+
+
+        //then
+
+        assertThat(expected).isFalse(); // expected should be equal to false
+
+    }
+
+}
