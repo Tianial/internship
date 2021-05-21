9dc72a606ee8c339ab698af40c9d929dbb0f9b87
rosinetiani <rosine.tiani@parcaune.com>
2021-05-21 12:18:46 +0200

    Tests for calculator

--- springboot-demo/src/test/java/com/parcaune/demo/DemoApplicationTests.java
+++ springboot-demo/src/test/java/com/parcaune/demo/DemoApplicationTests.java
@@ -1,13 +1,34 @@
 package com.parcaune.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-	@Test
-	void contextLoads() {
+//@SpringBootTest
+class CalculatorTest {
+
+
+	// creating an instance of the class
+		Calculator underTest= new Calculator();
+		@Test // tells us that itshouldAddTwoNumbers() is a test method
+		void itshouldAddTwoNumbers(){
+
+			//given
+			int numberOne = 1;
+			int numberTwo= 3;
+			//when
+			int result=underTest.add(numberOne,numberTwo);
+
+			// Then
+			int expectedValue= 4;
+			assertThat(result).isEqualTo(expectedValue);
+
+		}
+
+		class Calculator {
+			int add(int a, int b) {
+				return a + b;
+			}
+		}
 	}
 
-}
