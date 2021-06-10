8e5364602104fc69a7d58e6eff8beb6c967b581d
rosinetiani <rosine.tiani@parcaune.com>
2021-06-10 17:24:56 +0200

    comment added

--- Studentsmanagerapp/src/app/banner/banner.component.spec.ts
+++ Studentsmanagerapp/src/app/banner/banner.component.spec.ts
@@ -1,8 +1,9 @@
-import {ComponentFixture, ComponentFixtureAutoDetect, TestBed} from '@angular/core/testing';
+import {ComponentFixture, ComponentFixtureAutoDetect, async, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {BannerComponent} from './banner.component';
+import {FormsModule} from "@angular/forms";
 
-fdescribe('BannerComponent', () => {
+describe('BannerComponent', () => {
   let component: BannerComponent;
   let fixture: ComponentFixture<BannerComponent>;
   let h1: HTMLElement;
@@ -12,18 +13,21 @@ fdescribe('BannerComponent', () => {
     //Testbed used to configure the testing environment and configurE the testing module.
     // Allows overriding default providers, directives, pipes, modules of the test injector, which are defined in test_injector.js
     await TestBed.configureTestingModule({
-      declarations: [BannerComponent],  // BannerComponent is the main component we want to have in this testing environment.
-      providers: [   // Then add ComponentFixtureAutoDetect to the providers array of the testing module configuration for Automatic change detection
-        { provide: ComponentFixtureAutoDetect, useValue: true }
-      ]
+      imports: [
+        FormsModule
+      ],
+      declarations: [BannerComponent]  // BannerComponent is the main component we want to have in this testing environment.
+     // providers: [   // Then add ComponentFixtureAutoDetect to the providers array of the testing module configuration for Automatic change detection
+     //   { provide: ComponentFixtureAutoDetect, useValue: true }
+     // ]
     })
       .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BannerComponent); // Creates component
-    h1 = fixture.nativeElement.querySelector('h1');
-    component = fixture.componentInstance; //
+    h1 = fixture.nativeElement.querySelector('h1');// selects the title in the html and saves it in the the variable h1
+    component = fixture.componentInstance; //initialising the component to be an instance of a class
   });
 
   it('should create', () => {
@@ -38,7 +42,7 @@ fdescribe('BannerComponent', () => {
 
   it('no title in the html(DOM) after createComponent()', () => {
     expect(component.title).toEqual('banner works');
-    expect(h1.textContent).toEqual('');
+    expect(h1.textContent).toEqual(''); // by default there is is no title in html file
   });
 
   it('should display original title after detectChanges() in html(DOM)', () => {
@@ -46,5 +50,40 @@ fdescribe('BannerComponent', () => {
     fixture.detectChanges();
     expect(h1.textContent).toEqual('banner works');
   });
+
+  it('should  togglt the message', () => {
+    expect(component.hideContent).toBeTruthy();
+    component.toggle();
+    expect(component.hideContent).toBeFalsy();
+
+  });
+
+
+  it('should convert hero name to Title Case', waitForAsync(() => {
+    fixture.detectChanges();  // ngOnInit()
+
+    fixture.whenStable().then(() => {
+      // get the name's input and display elements from the DOM
+      const hostElement: HTMLElement = fixture.nativeElement;
+      const nameInput: HTMLInputElement = hostElement.querySelector('input');
+      const nameDisplay: HTMLElement = hostElement.querySelector('span'); // element selector
+
+      // simulate user entering a new name into the input box
+      nameInput.value = 'Tiani';
+
+      // Dispatch a DOM event so that Angular learns of input value change.
+      // In older browsers, such as IE, you might need a CustomEvent instead. See
+      // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
+      nameInput.dispatchEvent(new Event('input'));
+
+      // Tell Angular to update the display binding through the title pipe
+      fixture.detectChanges();
+
+      console.log({nameDisplay});
+      console.log({component: component.name});
+
+      expect(nameDisplay.textContent).toBe('Tiani');
+    });
+  }));
 
 });
--- Studentsmanagerapp/src/app/banner/banner.component.ts
+++ Studentsmanagerapp/src/app/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -7,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent {
 
-  title = 'banner works'
+  title = 'banner works';
+  hideContent = true;
+  severity = 450;
+  name: string;
 
-  todo() {
-
+  toggle() {
+    this.hideContent = !this.hideContent;
   }
+
+
 
 }
